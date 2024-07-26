ad26ea006586df3dfec6f2c011e44bc3de6f9381
Matheus N. Silva <matheuznsilva@yahoo.com.br>
2024-07-26 04:55:46 -0300

    c0 - First Commit

---
+++ TBBT_Jokenpo.py
@@ -0,0 +1,48 @@
+from random import randint
+
+def user_interface(options):
+    for index, options in enumerate(options):
+        print(f'{index} = {options}')
+    return int(input('Digite sua escolha? '))
+    
+def computer_choice(content):
+    return randint(0, len(content)-1)
+    
+def check_result(choices, player, computer, rules):
+    if player == computer:
+        return 'Empate'
+    
+    if choices[computer] in rules[choices[player]]:
+        return 'Jogador ganhou'
+
+    return 'Computador ganhou'
+
+def play():
+    print('\nBem vindo ao Pedra, Papel, Tesoura, Lagarto, Spock\n\n')
+
+    optionsList = ['Pedra', 'Papel', 'Tesoura', 'Lagarto', 'Spock']
+
+    rules = {
+        'Pedra': ['Tesoura', 'Lagarto'],
+        'Papel': ['Pedra', 'Spock'],
+        'Tesoura': ['Papel', 'Lagarto'],
+        'Lagarto': ['Spock', 'Papel'],
+        'Spock': ['Tesoura', 'Pedra'],
+    }
+
+    playerResult = user_interface(optionsList)
+    computerResult = computer_choice(optionsList)
+
+    print(f'Escolha do jogador: {optionsList[playerResult]}')
+    print(f'Escolha do computador: {optionsList[computerResult]}')
+
+    print(f'\n{check_result(optionsList, playerResult, computerResult, rules)}')
+
+def main():
+    playAgain = ''
+    while playAgain.lower() != 'n':
+        play()
+        print(f'Deseja jogar novamente? ')
+        playAgain = input('type \'S\' para sim ou \'n\' para não: ')
+
+main()
